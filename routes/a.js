774b9ebca620f15af9cc5f3b49746068eb7133fc
harisshk <hari850800@gmail.com>
2021-05-22 19:49:02 +0530

    1

--- routes/a.js
+++ routes/a.js
@@ -75,6 +75,8 @@ router.get('/a/:start/:end', async(req,res)=>{
 const formUrlEncoded = x =>
    Object.keys(x).reduce((p, c) => p + `&${c}=${encodeURIComponent(x[c])}`, '')
 
-
+router.get("",(res,req)=>{
+  res.send("Hello")
+})
         
 module.exports= router;
